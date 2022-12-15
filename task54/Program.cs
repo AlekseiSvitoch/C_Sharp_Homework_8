29dd2f483cc556cdd3081a44804875ae74ec900f
Aleksei Svitoch <pndlm@yandex.ru>
2022-12-15 20:01:09 +0600

    task 54 complete

--- task54/Program.cs
+++ task54/Program.cs
@@ -45,7 +45,9 @@ void OrderTheMatrix(int[,] matrix, int row, int col)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
+        {
             Console.Write($"{matrix[i, j]} \t");
+        }
         Console.WriteLine();
     }
 }
